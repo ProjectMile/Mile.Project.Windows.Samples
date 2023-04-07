f52887d5788d667cdfdbe886c6e8e0c1652f621e
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-08 00:39:22 +0800

    Add Mile.Samples.DynamicLibrary project.

---
+++ Mile.Samples.DynamicLibrary/Mile.Samples.DynamicLibrary.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Samples.DynamicLibrary
+ * FILE:      Mile.Samples.DynamicLibrary.cpp
+ * PURPOSE:   Implementation for Mile.Samples.DynamicLibrary
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileSamplesTestFunction()
+{
+    return 0;
+}
