bb17a48d6724b11878cd8f43b5686fb7b9f58efe
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-08 00:35:30 +0800

    Add Mile.Samples.StaticLibrary project.

---
+++ Mile.Samples.StaticLibrary/Mile.Samples.StaticLibrary.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Samples.StaticLibrary
+ * FILE:      Mile.Samples.StaticLibrary.cpp
+ * PURPOSE:   Implementation for Mile.Samples.StaticLibrary
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileSamplesTestFunction()
+{
+    return 0;
+}
