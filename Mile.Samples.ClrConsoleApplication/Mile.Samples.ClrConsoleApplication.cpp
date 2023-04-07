eebfb9d29eff90ae5b72f13cc97e463e88e16e46
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-08 01:13:19 +0800

    Add Mile.Samples.ClrConsoleApplicatio project.

---
+++ Mile.Samples.ClrConsoleApplication/Mile.Samples.ClrConsoleApplication.cpp
@@ -0,0 +1,27 @@
+﻿/*
+ * PROJECT:   Mile.Samples.ClrConsoleApplication
+ * FILE:      Mile.Samples.ClrConsoleApplication.cpp
+ * PURPOSE:   Implementation for Mile.Samples.ClrConsoleApplication
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+using namespace System;
+
+int main(array<System::String^>^ args)
+{
+    args;
+
+    Console::WriteLine(
+        "Mile.Samples.ClrConsoleApplication");
+    Console::WriteLine(
+        "================================================================");
+    Console::WriteLine(
+        "他喵的 MSVC 2019 工具集坑了我们价值百万美刀的项目！");
+    Console::WriteLine(
+        "The F@cking MSVC 2019 toolset ruined our $1M project!");
+
+    return 0;
+}
